04cf832fcdaf790871b0d2f0c2fe1bfcd2b76b9b
Ankit Gandhi <ankitgandhi452@gmail.com>
2023-03-18 02:59:44 +0530

    Service worker file beautified

--- public/serviceWorkerRegistration.js
+++ public/serviceWorkerRegistration.js
@@ -1,8 +1,9 @@
 if (navigator.serviceWorker) {
-  console.log('import.meta.url', import.meta.url)
-  navigator.serviceWorker.register(new URL('service-worker.js', import.meta.url), { type: 'module' }).then(function (registration) {
-    console.log('ServiceWorker registration successful with scope:', registration)
-  }).catch(function (error) {
-    console.log('ServiceWorker registration failed:', error)
-  })
+  navigator.serviceWorker
+    .register(new URL('service-worker.js', import.meta.url), { type: 'module' })
+    .then(function (registration) {
+      console.log('ServiceWorker registration successful')
+    }).catch(function (error) {
+      console.log('ServiceWorker registration failed:', error)
+    })
 }
