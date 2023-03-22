31e1c3da1b9c2a7be8e453a79d6a50490ea21ee4
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-03-22 18:51:17 +0530

    Fix WebHttp Commented Code

--- src/Configurations/WebHttp.js
+++ src/Configurations/WebHttp.js
@@ -1,6 +1,5 @@
-// import WebHttp from '@am92/web-http'
-// import CONTEXT from '@am92/web-http/CONTEXT'
-// import { AS_ENABLE_CRPTOGRAPHY, AS_API_KEY, AS_API_TIMEOUT, AS_API_DOMAIN } from './env'
+// import WebHttp, { CONTEXT } from '@am92/web-http'
+// import { AS_API_DOMAIN, AS_API_KEY, AS_ENABLE_CRPTOGRAPHY, AS_API_TIMEOUT } from './env'
 
 // const AS_AXIOS_HTTP_CONFIG = { baseURL: AS_API_DOMAIN, timeout: AS_API_TIMEOUT }
 // const AS_WEB_HTTP_CONFIG = { disableCrypto: !AS_ENABLE_CRPTOGRAPHY }
